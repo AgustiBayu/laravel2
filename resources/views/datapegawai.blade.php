a7980a2e303aebc8a6b579e473a63df19a4da96d
Agusti Bayu <Agustibayu.ABS@gmail.com>
2022-01-24 20:08:16 +0700

    6# Memberikan message pada tombol

--- resources/views/datapegawai.blade.php
+++ resources/views/datapegawai.blade.php
@@ -49,7 +49,7 @@
                         <td>{{ $row->created_at->format('D M Y')}}</td>
                         <td>
                         <a href="/tampilkandata/{{ $row->id }}" class="btn btn-warning">Edit</a>
-                        <a href="/delete/{{ $row->id }}" class="btn btn-danger">Delete</a>
+                        <a href="#" class="btn btn-danger delete" data-id="{{ $row->id }}" data-nama="{{ $row->nama }}">Delete</a>
                         </td>
                       </tr>
                     @endforeach
@@ -62,11 +62,38 @@
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script
+		src="https://code.jquery.com/jquery-3.6.0.slim.js"
+		integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY="
+	crossorigin="anonymous"></script>
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
   </body>
+  <script>
+      $('.delete').click(function() {
+            var pegawaiid = $(this).attr('data-id');
+            var nama = $(this).attr('data-nama');
+        swal({
+            title: "Yakin?",
+            text: "Kamu Akan menghapus data Pegawai dengan nama "+nama+" ",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+                window.location = "/delete/"+pegawaiid+""
+              swal("Data berhasil dihapus", {
+                icon: "success",
+              });
+            } else {
+              swal("Data tidak jadi di hapus");
+            }
+          });
+      });
+  </script>
 </html>
